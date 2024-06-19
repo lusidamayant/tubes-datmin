261f612f67c0f5cf5510c06061d17a0c905c2380
lusidamayant <slumbunglusi@gmail.com>
2024-06-19 18:17:57 +0700

    datmin

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // User::factory(10)->create();
+        User::create([
+            "uid" => "superadmin",
+            "name" => "superadmin",
+            "email" => "superadmin@bteam.site",
+            "password" => Hash::make("123456"),
+            "role_id" => "1",
+            "status" => "Aktif",
+            "email_verified_at" => Date::now()
+        ]);
+    }
+}
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,12 @@
+<li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
+    <a class="nav-link" href="#">
+        <i class="fas fa-tachometer-alt"></i><span>Dashboard</span>
+    </a>
+</li>
+<li class="dropdown">
+    <a href="#" class="nav-link has-dropdown"><i class="fas fa-lock"></i><span>Role and Permissions</span></a>
+    <ul class="dropdown-menu" style="display: none;">
+    <li><a class="nav-link" href="index-0.html">Role</a></li>
+    <li class="active"><a class="nav-link" href="index.html">Permissions</a></li>
+    </ul>
+</li>
